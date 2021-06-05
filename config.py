25d037a294398c056ed3246b5debd36c038ad206
Lakisor <65020235+Lakisor@users.noreply.github.com>
2021-06-06 01:02:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'NzI3NTE5MjkwMDcyNDk4MjA2.XvtBIw.Q0VctBuFQHllnSSRFY0u99uVfvU',
-    'bot': 'QQBOT',
-    'id': 727519290072498206,
-    'prefix': '~'
+    'token': 'TOKEN',
+    'bot': 'BOT',
+    'id': id,
+    'prefix': 'prefix'
 }
